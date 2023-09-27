a01c4e718ee3f29dd1b11fa26e98b504c192f205
Chethan B N <47515084+Chethan30@users.noreply.github.com>
2023-09-26 20:18:36 -0500

    Laplace Smoothening for uni abd bigram LMs

--- Assignment 1 - Language Modelling/main.py
+++ Assignment 1 - Language Modelling/main.py
@@ -64,12 +64,40 @@ def get_unked_unigram_perplexity(unigram_prob, val_tokens):
     N = len(val_tokens)
     log_sum = 0
     for token in val_tokens:
-        if token in unigram_prob:
+        if token in unigram_prob.keys():
             log_sum += np.log2(unigram_prob[token])
         else:
             log_sum += np.log2(unigram_prob["<unk>"])
     perplexity = np.power(2, -log_sum/N)
     return perplexity
+
+def get_unked_bigram_perplexity(bigram_prob, unigram_prob, val_tokens):
+    N = len(val_tokens)
+    log_sum = 0
+    for i in range(1, len(val_tokens)):
+        current_bigram = (val_tokens[i-1], val_tokens[i])
+        if current_bigram in bigram_prob:
+            log_sum += np.log2(bigram_prob[current_bigram])
+        else:
+            log_sum += np.log2(bigram_prob[("<unk>", "<unk>")])
+    perplexity = np.power(2, -log_sum/N)
+    return perplexity
+
+
+def get_addK_laplace_unigram_smoothing(k, total_word_count, unigram_count, vocab_size):
+    unigram_prob = {}
+    for token, count in unigram_count.items() :
+        unigram_prob[token] = (count + k) / (total_word_count + (k * vocab_size))
+    return unigram_prob
+
+def get_addK_laplace_bigram_smoothing(k, bigram_count, unigram_count, vocab_size):
+    bigram_prob = {}
+    for token, count in bigram_count.items() :
+        first_word = token[0]
+        word_count = unigram_count[first_word]
+        bigram_prob[token] = (count + k) / (word_count + (k * vocab_size))
+    return bigram_prob
+
 
 
 def main():
@@ -94,32 +122,38 @@ def main():
     unigram_count = get_unigram_counts(input_tokens)
     vocab_size = len(unigram_count.keys())
     unigram_prob = get_unigram_probability(n_gram_count= unigram_count, total_word_count= total_word_count)
+    # unigram_perplexity = get_unigram_perplexity(unigram_prob= unigram_prob, val_tokens= val_tokens)
+    # print(unigram_perplexity)
 
     bigram_count = get_bigram_count(input_tokens)
     bigram_prob = get_bigram_probability(bigram_count= bigram_count, unigram_count= unigram_count)
 
-    # TODO: Calculate perplexity for bigram models
-
-    # unigram_perplexity = get_unigram_perplexity(unigram_prob= unigram_prob, val_tokens= val_tokens)
-    # print(unigram_perplexity)
-
-    # print(sorted(unigram_count.values()))
-    min_frequency = 5
+    # <----------------Handling Unknown Words based on minimum frequency ----------------------->
+    min_frequency = 1
     unk_words = get_list_of_unk(min_frequency, unigram_count)
     unk_input_tokens = get_unked_tokens(unk_words, input_tokens)
+    unk_vocab_size = len(set(unk_input_tokens))
 
     unk_unigram_count = get_unigram_counts(unk_input_tokens)
     unk_unigram_prob = get_unigram_probability(n_gram_count= unk_unigram_count, total_word_count= total_word_count)
 
     unk_bigram_count = get_bigram_count(unk_input_tokens)
-    unk_bigram_prob = get_bigram_probability(bigram_count= unk_bigram_count, unigram_count= unk_unigram_count)
-
-    unigram_perplexity = get_unked_unigram_perplexity(unigram_prob= unk_unigram_prob, val_tokens= val_tokens)
+    # Not possile without smoothening 
+    # unk_bigram_prob = get_bigram_probability(bigram_count= unk_bigram_count, unigram_count= unk_unigram_count)
+    unk_bigram_prob = get_addK_laplace_bigram_smoothing(k=1, bigram_count=unk_bigram_count, unigram_count= unk_unigram_count, vocab_size= unk_vocab_size)
+    # print(unk_bigram_count)
+    # print(unk_bigram_prob)
+    
+    # unigram_perplexity = get_unked_unigram_perplexity(unigram_prob= unk_unigram_prob, val_tokens= val_tokens)
+    # addOne_laplace_smoothed_unigram_prob = get_addK_laplace_unigram_smoothing(k=0.01, total_word_count= total_word_count, unigram_count= unk_unigram_count, vocab_size= unk_vocab_size)
+    # addOne_laplace_smoothed_unigram_perplexity = get_unked_unigram_perplexity(unigram_prob= addOne_laplace_smoothed_unigram_prob, val_tokens= val_tokens)
     # print(unigram_perplexity)
+    # print(addOne_laplace_smoothed_unigram_perplexity)
 
-    # TODO: Calculate perplexity for bigram models with UNK
+    bigram_perplexity = get_unked_bigram_perplexity(bigram_prob= unk_bigram_prob, unigram_prob=unk_unigram_prob , val_tokens= val_tokens)
+    print(bigram_perplexity)
 
-
+    # TODO: Other Smoothening Methods
 
 
 
